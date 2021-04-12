ca63470428e1a29d17df610fa2fc44a9a48b3246
YNotes <65722992+YNseries@users.noreply.github.com>
2021-04-12 23:24:41 +0900

    20201222

---
+++ JavaBasicProject1/src/MainClass.java
@@ -0,0 +1,32 @@
+// 주석 : 프로그램에서 실행되지 않는 부분 (순서...필요한 내용 정리)
+// 한줄 주석 
+/*
+ * 여러줄 주석이 가능
+ */
+// 자바 문법
+// 실행하는 과정
+// 단축키
+//ㅁㄴㅇㄻㅎㄴㅁㄴㅇㅎ
+/*ㅁㄴㅇㄹㄶㄴㅇㅎㄴㅂ
+ㄴasdgqsf
+sdgqwfsf*/
+// 여러줄 주석: 드래그 후 ctrl+shift+/
+// 주석 해제: 드래그 후 ctrl+shift+\
+
+public class MainClass {
+	/*
+	 * public static void main(String[] args)
+	 * 
+	 * }
+	 */
+	
+	/*public static void main(String[] args) {
+		// "main"+ctrl+space
+*/		
+	public static void main(String[] args) {
+		System.out.println("Hello");
+		// "sysout"+ctrl+space
+	}
+}
+// JDK => path, classpath
+// 이클립스 => 구동하는 방법 (자바 셋팅)
